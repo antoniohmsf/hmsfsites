68b6ba9ac2903fc3cd0758cd3de65cc60b8837ce
antoniohmsf <106127117+antoniohmsf@users.noreply.github.com>
2023-05-31 00:11:13 -0300

    v1
    new

---
+++ scprit.js
@@ -0,0 +1,18 @@
+
+    function sim(){
+        alert("Se você clicou aqui é porque também está com saudade dos nossos momentos divertidos. Que tal fazermos um brownie e umas batatinhas para curtir um pouco? :)")
+    }
+
+    function desvia(t){
+        var btn = t;
+        btn.style.position="absolute";
+        btn.style.bottom= geraPosição(10,90);
+        btn.style.left= geraPosição(10,90);
+        console.log(btn);
+        console.log("Opa, desviei...");
+    }
+
+    function geraPosição(min, max) {
+        return Math.random() * ((max - min) + min) + "%";
+    }
+
